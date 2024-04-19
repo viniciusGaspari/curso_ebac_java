ae39a8ee41e3e603675c9884bfe786320506cf66
viniciusGaspari <vinicius7degaspari@gmail.com>
2024-04-19 00:38:54 -0300

    terafa calcula da média

---
+++ MainClass.java
@@ -0,0 +1,15 @@
+package CalcMedia;
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+
+		int nota1 = 4;
+		double nota2 = 1.54;
+		int nota3 = 34;
+		int nota4 = 567;
+		
+		double soma = (nota1  + nota2) + (nota3 + nota4) / 4;
+		System.out.println(soma);
+	}
+}
