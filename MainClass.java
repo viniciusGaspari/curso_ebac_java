cfe3feec2a76147f36a242ce6cdc24d93877f3ac
viniciusGaspari <vinicius7degaspari@gmail.com>
2024-04-20 02:26:20 -0300

    tarefa_casting_wrapper_autboxing_unboxing

---
+++ MainClass.java
@@ -0,0 +1,19 @@
+package BoxingAutoBoxingCasting;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+	double d1 = 1.23;
+	float f1 = (float) d1;
+	
+	float f2 = 1.245f;
+	double d2 = f2;
+	
+	int it1 = Integer.valueOf(132);
+	System.out.println(it1);
+	
+		
+	}
+	
+}
